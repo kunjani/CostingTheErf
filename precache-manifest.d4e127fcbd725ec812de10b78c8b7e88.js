ee207526412f487f24c1cc8ee36183040383bf0b
kunjani <chris.sadler@gmail.com>
2019-02-02 17:46:35 +0000

    Add files via upload

---
+++ precache-manifest.d4e127fcbd725ec812de10b78c8b7e88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CostingTheErf/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "08a41d36a5381b792311",
+    "url": "/CostingTheErf/static/js/runtime~main.08a41d36.js"
+  },
+  {
+    "revision": "2787fcfa4fb743fa8961",
+    "url": "/CostingTheErf/static/js/main.2787fcfa.chunk.js"
+  },
+  {
+    "revision": "80a7a5fdb865c3a265ae",
+    "url": "/CostingTheErf/static/js/1.80a7a5fd.chunk.js"
+  },
+  {
+    "revision": "2787fcfa4fb743fa8961",
+    "url": "/CostingTheErf/static/css/main.e2051247.chunk.css"
+  },
+  {
+    "revision": "d44351ae68c161896794ded5a4746a9b",
+    "url": "/CostingTheErf/index.html"
+  }
+];
